cccf505b61a2cbae6f3400c0871ad4aec7a128e4
Sofrin <85531415+sofrin@users.noreply.github.com>
2026-03-13 17:16:41 +0700

    upd deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
 	build: {
-		rollupOptions: {
+		rolldownOptions: {
 			external: ["bun", "bun:*", "@prisma/client"],
 		},
 	},
-	esbuild: {
-		drop: process.env.NODE_ENV === "production" ? ["console", "debugger"] : [],
+	resolve: {
+		tsconfigPaths: true,
 	},
-	plugins: [
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-	],
+	plugins: [tailwindcss(), tanstackStart(), viteReact()],
 });
 
 export default config;

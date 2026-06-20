4f56188a81a8b1529cbb48a74d0a8a1dc3d3b0a0
Sofrin <85531415+sofrin@users.noreply.github.com>
2026-06-20 07:35:36 +0700

    Update $id.ts

--- src/routes/$id.ts
+++ src/routes/$id.ts
@@ -7,7 +7,7 @@ import { db } from "@/lib/prisma.ts";
 export const Route = createFileRoute("/$id")({
 	server: {
 		handlers: {
-			GET: async ({ params }) => {
+			GET: async ({ params, request }) => {
 				const { id: idRaw } = params;
 				const id = idRaw.split(".")[0];
 				console.log("id:", id);
@@ -31,12 +31,35 @@ export const Route = createFileRoute("/$id")({
 						contentDisposition: "inline",
 					});
 					console.log(`file.type:  `, file.type);
+					const rangeHeader = request.headers.get("range");
+					const { size, type } = await s3file.stat();
+					if (rangeHeader) {
+						// Parse incoming header format: "bytes=start-end"
+						const parts = rangeHeader.replace(/bytes=/, "").split("-");
+						console.log(`parts:  `, parts);
+						const start = parseInt(parts[0], 10);
+						const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+						console.log(`start:  `, start);
+						console.log(`end:  `, end);
+						const contentLength = end - start + 1;
+						console.log(`contentLength:  `, contentLength);
+						// S3File.slice handles the range request to the cloud provider
+						const fileSlice = s3file.slice(start, end + 1);
 
+						return new Response(fileSlice.stream(), {
+							headers: {
+								"Accept-Ranges": "bytes",
+								"Content-Length": contentLength.toString(),
+								"Content-Range": `bytes ${start}-${end}/${size}`,
+								"Content-Type": type,
+							},
+							status: 206,
+						});
+					}
 					return new Response(s3file.stream(), {
 						headers: {
 							"Accept-Ranges": "bytes",
 							"Content-Length": file.size.toString(),
-							"Content-Range": `bytes 0-${file.size.toString()}/${file.size.toString()}`,
 							"Content-Type": file.type,
 						},
 					});
